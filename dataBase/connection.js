ae2ed2bb677e182c0c832cd82dad542eedb9ab63
KurtXiao <zechuan.xiao@vanderbilt.edu>
2022-01-18 20:57:09 -0600

    init

--- dataBase/connection.js
+++ dataBase/connection.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql');
 
 let connection = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'root',
+    host     : 'todolistdatabase.cwwgvitfh1as.us-east-1.rds.amazonaws.com',
+    user     : 'kurtxiao',
     password : 'huZ87sbe',
     database: 'todolist',
+    port: '3306',
 });
    
 connection.connect(function(err) {
