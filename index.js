cbbe4c249d6769a9b0a9826a36f6b9072f6d817f
KurtXiao <zechuan.xiao@vanderbilt.edu>
2022-01-19 01:33:08 -0600

    fix port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
-const port = 443;
+const port = 8080;
 
 const dBOptions = require('./dataBase/index');
 
