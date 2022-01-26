0183dd9cc17d919cf1dbecbf2b0f545127806c3f
KurtXiao <zechuan.xiao@vanderbilt.edu>
2022-01-26 09:41:03 -0600

    remove .env

--- dataBase/connection.js
+++ dataBase/connection.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql');
 
 let connection = mysql.createConnection({
-    host     : process.env.HOST,
-    user     : process.env.USER,
-    password : process.env.PASSWORD,
-    database: process.env.DATABASE,
-    port: process.env.PORT,
+    host     : 'todolistdatabase.cwwgvitfh1as.us-east-1.rds.amazonaws.com',
+    user     : 'kurtxiao',
+    password : 'huZ87sbe',
+    database: 'todolist',
+    port: '3306',
 });
    
 connection.connect(function(err) {
